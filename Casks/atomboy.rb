8e78056804962998b6b155b8cca7b3dfb8ecd01e
William Martin <maartz@protonmail.com>
2026-07-29 23:33:57 +0200

    atomboy 0.3.0: app rebuilt with X/Y save-load on the gamepad

--- Casks/atomboy.rb
+++ Casks/atomboy.rb
@@ -1,6 +1,6 @@
 cask "atomboy" do
   version "0.3.0"
-  sha256 "93bf8823db9971e3a80598e3f5e3739202506844133cd77cfb3d0d7d3a058403"
+  sha256 "c9d6321d524616c2d88def56238abe9e7ce9f282a82991c226286803c989f550"
 
   url "https://github.com/Maartz/atomboy/releases/download/v#{version}/Atomboy.app.zip"
   name "Atomboy"
